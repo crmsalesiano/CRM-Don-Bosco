2cb8c74952cc0e686e84dc12b2514b87b8d94c08
CRM Don Bosco <crmsalesiano@gmail.com>
2026-09-19 19:30:06 -0600

    chore: agregar credenciales de Supabase

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@
 // ============================================================
 
 window.APP_CONFIG = {
-  SUPABASE_URL: "PEGA_AQUI_TU_SUPABASE_URL",
-  SUPABASE_ANON_KEY: "PEGA_AQUI_TU_SUPABASE_ANON_KEY"
+  SUPABASE_URL: "https://mmxehezdokhpvivaqaso.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1teGVoZXpkb2tocHZpdmFxYXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4NTc3MDUsImV4cCI6MjEwNTQzMzcwNX0.14xzZCnx0BcHzyCgQrDzAv8rHXDYQ24Aawp2vRBWIBY"
 };
